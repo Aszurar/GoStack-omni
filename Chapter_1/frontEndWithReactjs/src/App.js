7e036c4ac8ddabd26fa8428c4982fc582b68c6c9
Lucas de Lima <lms.souza39@gmail.com.br>
2022-05-06 22:07:00 -0300

    :tada: reactjs concepts :tada:

--- Chapter_1/frontEndWithReactjs/src/App.js
+++ Chapter_1/frontEndWithReactjs/src/App.js
@@ -1,20 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from './components/Header';
 import './App.css';
 import ImageTest from '../assets/hzd.jpg'
+import api from './services/api'
 
 function App(){
   const [projects, setProjects] = useState(['Desenvolvimendo um app', 'Desenvolvimendo um site']);
 
-  function handleAddProject(){
-      setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    useEffect(() => {
+      api.get('projects').then(response => {
+        setProjects(response.data);
+      });
+    }, [])
+
+    async function handleAddProject(){
+      // setProjects([...projects, `Novo projeto ${Date.now()}`]);
+      try{
+        const response = await api.post('/projects', {
+          title: `Novo projeto ${Date.now()}`,
+          owner: 'Lucas de Lima'
+        })
+          setProjects([...projects, response.data]);
+      } catch(err){
+        console.log(err);
+      }
+
     }
-  return (
+
+    return (
     <>
-      <img width={400} src={ImageTest} alt="imagem de teste"/>
+      {/* <img width={400} src={ImageTest} alt="imagem de teste"/> */}
       <Header title="ReactJS"/>
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
       
       <button onClick={handleAddProject}>Adicionar um novo projeto</button>
