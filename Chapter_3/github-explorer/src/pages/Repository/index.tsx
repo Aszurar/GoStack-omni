c54dfcad10fee0b80459c79fbfaa150fabea98ed
Lucas de Lima <lms.souza39@gmail.com.br>
2022-05-22 21:24:01 -0300

    :fire: routes complete! learning about react-router-dom :fire:

---
+++ Chapter_3/github-explorer/src/pages/Repository/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export const Repository: React.FC = () => <div>Repository</div>;
---
+++ Chapter_3/github-explorer/src/router/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes as Router } from 'react-router-dom';
+import { Dashboard } from '../pages/Dashboard';
+import { Repository } from '../pages/Repository';
+
+export function Routes() {
+  return (
+    <Router>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/repository" element={<Repository />} />
+    </Router>
+  );
+}
